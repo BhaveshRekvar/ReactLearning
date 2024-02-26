4da486ff6f880b801bf02d3b4803edd5f3c8f2e6
Bhavesh Rekvar <bhaveshrekvar005@gmail.com>
2024-02-26 18:55:09 +0530

    create background color change project using tailwind library

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+import './App.css';
+import { BackgroundChange } from './Components/BackgroundChange';
+
 export default () => {
   return (
     <>
-      <h1 style={{ textAlign: "center" }}>React Learning </h1>
+      {/* <h1 style={{ textAlign: "center" }} className='flex text-3xl font-bold bg-gray-500 text-white rounded-xl p-4 m-4'>React Learning </h1> */}
+      <BackgroundChange />
     </>
   )
 }
